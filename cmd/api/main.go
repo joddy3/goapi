bd55722d3a28bf55de66e5ab37a4cbaa93566e5d
Joddy0 <143619313+Joddy0@users.noreply.github.com>
2026-04-26 15:11:08 +0700

    Initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi" // package untuk membuat REST API yang cukup fleksibel dan mudah digunakan
+	"github.com/joddy3/goapi/internal/handlers"
+	log "github.com/sirupsen/logrus" // package untuk log
+)
+
+func main() {
+	
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/joddy3/goapi
+
+go 1.25.7
