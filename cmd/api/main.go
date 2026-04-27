28ddfbe7820aadd01d9a25bc6dd1b369812ffbff
Joddy0 <143619313+Joddy0@users.noreply.github.com>
2026-04-27 14:25:12 +0700

    feat: implement get coin balance endpoint, authorization, and mock database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,11 +4,27 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/go-chi/chi" // package untuk membuat REST API yang cukup fleksibel dan mudah digunakan
+	"github.com/go-chi/chi"
 	"github.com/joddy3/goapi/internal/handlers"
-	log "github.com/sirupsen/logrus" // package untuk log
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	
+	log.SetReportCaller(true)
+	var r *chi.Mux = chi.NewRouter()
+	handlers.Handler(r)
+
+	fmt.Println("Starting GO API service...")
+
+	fmt.Println(`
+ ______     ______        ______     ______   __    
+/\  ___\   /\  __ \      /\  __ \   /\  == \ /\ \   
+\ \ \__ \  \ \ \/\ \     \ \  __ \  \ \  _-/ \ \ \  
+ \ \_____\  \ \_____\     \ \_\ \_\  \ \_\    \ \_\ 
+  \/_____/   \/_____/      \/_/\/_/   \/_/     \/_/ `)
+
+	err := http.ListenAndServe("localhost:8000", r)
+	if err != nil {
+		log.Error(err)
+	}
 }
